7e981b3e910b01993d610d5eab799f92e492cf82
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2026-03-18 02:08:32 +0530

    feat: update headers for background remover route to enhance security policies

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,20 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: "/background-remover",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+      {
+        source: "/background-remover/:path*",
         headers: [
           {
             key: "Cross-Origin-Opener-Policy",
